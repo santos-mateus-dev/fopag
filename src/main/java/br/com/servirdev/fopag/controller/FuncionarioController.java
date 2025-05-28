b09b24fafcb2e250952b826d2d47f020a634ea94
Mateus-Tester <matteussb@outlook.com>
2025-05-28 13:13:21 -0300

    Inclui ajustes na classe FuncionarioController

--- src/main/java/br/com/servirdev/fopag/controller/FuncionarioController.java
+++ src/main/java/br/com/servirdev/fopag/controller/FuncionarioController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -20,27 +22,37 @@ public class FuncionarioController {
 
     @PostMapping
     @Transactional
-    public void cadastrarFuncionario(@RequestBody @Valid DadosCadastroFuncionario dados){
-        repository.save(new Funcionario(dados));
+    public ResponseEntity cadastrarFuncionario(@RequestBody @Valid DadosCadastroFuncionario dados, UriComponentsBuilder uriBuilder){
+        var funcionario = new Funcionario(dados);
+        repository.save(funcionario);
+
+        var uri = uriBuilder.path("/funcionarios/{id}").buildAndExpand(funcionario.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoFuncionario(funcionario));
     }
 
     @GetMapping
-    public Page<DadosListagemFuncionario> listarFuncionario(@PageableDefault(size=10, sort={"nome"}) Pageable paginacao){
-        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemFuncionario::new);
+    public ResponseEntity <Page<DadosListagemFuncionario>> listarFuncionario(@PageableDefault(size=10, sort={"nome"}) Pageable paginacao){
+        var page = repository.findAllByAtivoTrue(paginacao).map(DadosListagemFuncionario::new);
+        return ResponseEntity.ok(page);
+
     }
 
     @PutMapping
     @Transactional
-    public void atualizarFuncionario(@RequestBody @Valid DadosAtualizacaoFuncionario dados){
+    public ResponseEntity atualizarFuncionario(@RequestBody @Valid DadosAtualizacaoFuncionario dados){
         var funcionario = repository.getReferenceById(dados.id());
         funcionario.atualizarInformacoes(dados);
+
+        return ResponseEntity.ok(new DadosDetalhamentoFuncionario(funcionario));
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void inativarFuncionario(@PathVariable Long id){
+    public ResponseEntity inativarFuncionario(@PathVariable Long id){
         var funcionario = repository.getReferenceById(id);
         funcionario.inativar();
 
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/br/com/servirdev/fopag/funcionario/DadosDetalhamentoFuncionario.java
+++ src/main/java/br/com/servirdev/fopag/funcionario/DadosDetalhamentoFuncionario.java
@@ -1,4 +1,18 @@
 package br.com.servirdev.fopag.funcionario;
 
-public record DadosDetalhamentoFuncionario() {
+import br.com.servirdev.fopag.endereco.DadosEndereco;
+import br.com.servirdev.fopag.endereco.Endereco;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+import java.time.LocalDate;
+
+public record DadosDetalhamentoFuncionario(Long id, String nome, String cpf, LocalDate dataNascimento, String pis, String email, Cargo cargo, Integer salario, LocalDate admissao, Endereco endereco ) {
+
+    public DadosDetalhamentoFuncionario(Funcionario funcionario){
+        this(funcionario.getId(), funcionario.getNome(), funcionario.getCpf(), funcionario.getDataNascimento(), funcionario.getPis(), funcionario.getEmail(), funcionario.getCargo(), funcionario.getSalario(), funcionario.getAdmissao(), funcionario.getEndereco());
+    }
 }
