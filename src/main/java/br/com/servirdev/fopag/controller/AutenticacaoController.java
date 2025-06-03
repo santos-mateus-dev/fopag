d0f408c7898b5d2e936fe7127e0abfa4a988c3ab
Mateus-Tester <matteussb@outlook.com>
2025-06-03 16:33:55 -0300

    Cria função de gerar token

--- src/main/java/br/com/servirdev/fopag/controller/AutenticacaoController.java
+++ src/main/java/br/com/servirdev/fopag/controller/AutenticacaoController.java
@@ -1,6 +1,8 @@
 package br.com.servirdev.fopag.controller;
 
 import br.com.servirdev.fopag.domain.usuario.DadosAutenticacao;
+import br.com.servirdev.fopag.domain.usuario.Usuario;
+import br.com.servirdev.fopag.infra.security.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,12 +20,15 @@ public class AutenticacaoController {
     @Autowired
     private AuthenticationManager manager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados){
         var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
         var authentication = manager.authenticate(token);
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(tokenService.gerarToken((Usuario) authentication.getPrincipal()));
     }
 
 }
---
+++ src/main/java/br/com/servirdev/fopag/infra/security/TokenService.java
@@ -0,0 +1,33 @@
+package br.com.servirdev.fopag.infra.security;
+
+import br.com.servirdev.fopag.domain.usuario.Usuario;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+
+@Service
+public class TokenService {
+
+    public String gerarToken(Usuario usuario) {
+        try {
+            var algoritimo = Algorithm.HMAC256("adm");
+            return JWT.create()
+                    .withIssuer("fopag")
+                    .withSubject(usuario.getLogin())
+                    .withExpiresAt(dataExpiracao())
+                    .sign(algoritimo);
+        } catch (JWTCreationException exception){
+            throw new RuntimeException("Erro ao gerar token jwt", exception);
+        }
+    }
+
+    private Instant dataExpiracao() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
