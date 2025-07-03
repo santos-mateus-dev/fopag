886d2d67a6017425895eb31a30ed50a047c485b7
Mateus-Tester <matteussb@outlook.com>
2025-07-03 13:10:51 -0300

    Cria filtro para buscar e validar token no cabeçalho

--- src/main/java/br/com/servirdev/fopag/infra/security/SecurityConfigurations.java
+++ src/main/java/br/com/servirdev/fopag/infra/security/SecurityConfigurations.java
@@ -2,6 +2,7 @@ package br.com.servirdev.fopag.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -18,7 +19,12 @@ public class SecurityConfigurations {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().build();
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .build();
     }
 
     @Bean
--- src/main/java/br/com/servirdev/fopag/infra/security/SecurityFilter.java
+++ src/main/java/br/com/servirdev/fopag/infra/security/SecurityFilter.java
@@ -1,10 +1,13 @@
 package br.com.servirdev.fopag.infra.security;
 
+import br.com.servirdev.fopag.domain.usuario.UsuarioRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -16,23 +19,42 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Autowired
     private TokenService tokenService;
 
+    @Autowired
+    private UsuarioRepository repository;
+
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
         var tokenJWT = recuperarToken(request);
-        var subject = tokenService.getSubject(tokenJWT);
 
+        if  (tokenJWT != null) {
+            /*Recupera o token*/
+            var subject = tokenService.getSubject(tokenJWT);
+            /*Recupera o token do cabeçalho e valida se está correto*/
+            var usuario = repository.findByLogin(subject);
+            /*Pega o e-mail do usuário, carrega o objeto usuário*/
+            var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        
         filterChain.doFilter(request, response);
 
     }
 
     private String recuperarToken(HttpServletRequest request) {
         var authorizationHeader = request.getHeader("Authorization");
-        if (authorizationHeader == null) {
+        if(authorizationHeader != null) {
+            return authorizationHeader.replace("Bearer", "");
+        }
+
+        return null;
+
+        /*if (authorizationHeader == null) {
             throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization");
         }
 
-        return authorizationHeader.replace("Bearer ", "");
+        return authorizationHeader.replace("Bearer ", "");*/
     }
 }
